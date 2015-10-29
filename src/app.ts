46b4082d6b5cbbbdb722eeff2ced327f4d3f68a4
Stuart Tannehill <stuart@litmusbox.com>
2015-10-28 23:17:17 -0400

    Switched 2-way data binding to minimalistic, real world usage. Cleaned up some typos.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-import {Component, bootstrap} from 'angular2/angular2';
+import {Component, bootstrap, FORM_DIRECTIVES} from 'angular2/angular2';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './dist/templates/main.html'
+    templateUrl: './dist/templates/main.html',
+    directives: [FORM_DIRECTIVES],
     // template: `<h1>Who's awesome? {{name}} is!</h1>
     // The winner is: <input [value]="name" (input)="name = $event.target.value">
-    // <p>(this was included fomr an inline template)</p>`
+    // <p>(this was included from an inline template)</p>`
 })
 
 class AppComponent {
